bb4bece28f2266de77866fab3c380d7b08dd58bf
villagrat <elsainetedelautrec@gmail.com>
2022-10-17 14:57:01 -0300

    add base Badge, Card & Button UI Components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,9 +1,10 @@
 <template>
   <section>FILTER</section>
   <section>
+    <base-card>
     <div class="controls">
-    <button>Refresh</button>
-    <router-link to="/register">Register as Coach</router-link>
+    <base-button mode="outline">Refresh</base-button>
+    <base-button link to="/register">Register as Coach</base-button>
   </div>
   <ul v-if="hasCoaches">
     <coach-item v-for="coach in filteredCoaches" 
@@ -15,6 +16,7 @@
     :areas="coach.areas"></coach-item>
   </ul>
   <h3 v-else>No coaches found.</h3>
+</base-card>
 </section>
 </template>
 
