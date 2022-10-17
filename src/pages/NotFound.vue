877dd377880121312e8c63374ac1b7800835708b
villagrat <elsainetedelautrec@gmail.com>
2022-10-17 14:29:26 -0300

    vuex init wire up

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,3 @@
 <template>
-  
+  <h2>Not Found...</h2>
 </template>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,3 +1,24 @@
 <template>
-  
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+    <button>Refresh</button>
+    <router-link to="/register">Register as Coach</router-link>
+  </div>
+  <ul>
+    <li v-for="coach in filteredCoaches" :key="coach.id">
+      {{ coach.firstName }}
+    </li>
+  </ul>
+</section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches(){
+      return this.$store.getters['coaches/coaches']
+    }
+  }
+}
+</script>
